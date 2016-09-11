b56c0003dafa50d1e6e4853ce7c29d97b916e7be
masakih <masakih@users.sourceforge.jp>
2016-09-11 10:52:59 +0900

    Initial Commit

---
+++ KanColleGraphicDivider/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KanColleGraphicDivider
+//
+//  Created by Hori,Masaki on 2016/09/11.
+//  Copyright © 2016年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
